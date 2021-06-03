dc1c247d40b049f5c1411d908f1a94da285657c5
Ivan Isakov <60196280+ivanisakov99@users.noreply.github.com>
2021-06-03 22:45:50 +0300

    Add files via upload

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Ivan Isakov.");
+            DateTime datetime = DateTime.Now;
+            Console.WriteLine("The current date and time is {0}.", datetime.ToString());
+        }
+    }
+}
