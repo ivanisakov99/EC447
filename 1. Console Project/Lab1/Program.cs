fff29424c3d61c698fc831c5d9c30ffa06b47867
Ivan Isakov <isakovi@bu.edu>
2021-06-25 19:50:15 +0300

    Update 1

--- 1. Console Project/Lab1/Program.cs
+++ 1. Console Project/Lab1/Program.cs
@@ -10,9 +10,14 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            // My name
             Console.WriteLine("My name is Ivan Isakov.");
+            // Get the current date and time
             DateTime datetime = DateTime.Now;
+            // Output the current date and time
             Console.WriteLine("The current date and time is {0}.", datetime.ToString());
+            // Keep the window open without debugging
+            Console.ReadLine();
         }
     }
 }
